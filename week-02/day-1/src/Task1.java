cd3b620ad8e1d3b2e1329ba0be550687a1d577b1
Viktor Salanki <salankiv5@gmail.com>
2017-09-11 14:02:25 +0200

    1-4 tasks ready

---
+++ week-02/day-1/src/Task1.java
@@ -0,0 +1,6 @@
+public class Task1 {
+    public static void main(String[] args) {
+    // Modify this program to greet you instead of the World!
+    System.out.println("Hello, Viktor!");
+    }
+}
