3666e525a81bb9f1c649fd7ded8c15fa8ab93f73
Viktor Salanki <salankiv5@gmail.com>
2017-09-12 13:51:05 +0200

    AppendA, Doubling, Greet exercises

---
+++ week-02/day-2/src/AppendA.java
@@ -0,0 +1,19 @@
+public class AppendA {
+    public static void main(String[] args) {
+    //  Create the usual class wrapper
+    //  and main method on your own.
+
+    // - Create a string variable named `am` and assign the value `kuty` to it
+    // - Write a function called `appendA` that gets a string as an input
+    //   and appends an 'a' character to its end
+    // - Print the result of `appendA(am)`
+
+    String am = "kuty";
+    System.out.println(appandA(am));
+    }
+
+    public static String appandA(String input) {
+        return input + "a";
+    }
+
+}
