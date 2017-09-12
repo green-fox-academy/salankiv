9757ba6c3fb61caafbbbfe91ad8fefadc53f8bad
Viktor Salanki <salankiv5@gmail.com>
2017-09-12 17:10:46 +0200

    DiagonalMatrix, IncrementElement, PrintAll exercises

---
+++ week-02/day-2/src/DiagonalMatrix.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+        // - Create (dynamically) a two dimensional array
+        //   with the following matrix. Use a loop!
+        //
+        //   1 0 0 0
+        //   0 1 0 0
+        //   0 0 1 0
+        //   0 0 0 1
+        //
+        // - Print this two dimensional array to the output
+
+        System.out.println("Give me a number:");
+        Scanner myScanner = new Scanner(System.in);
+        int userNumber = myScanner.nextInt();
+
+        String[][] matrix = new String[userNumber][userNumber];
+
+        for (int row = 0; row <= userNumber -1; row++) {
+            for (int col = 0; col <= userNumber - 1; col++) {
+                if (row == col) {
+                    matrix[row][col] = "1";
+                } else {
+                    matrix[row][col] = "0";
+                }
+            }
+        }
+
+        for (int i = 0; i <= matrix.length - 1; i++) {
+            String print = matrix[i][i];
+            for (int j = 0; j <= matrix.length - 1; j++) {
+                print = print + matrix[i][j];
+            }
+            System.out.println(print);
+        }
+
+    }
+}
