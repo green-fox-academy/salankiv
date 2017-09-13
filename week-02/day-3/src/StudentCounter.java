9377bcb8de97e3444159e5c1c348b92096d9fb20
Viktor Salanki <salankiv5@gmail.com>
2017-09-13 21:03:32 +0200

    Finalize StudentCounter

--- week-02/day-3/src/StudentCounter.java
+++ week-02/day-3/src/StudentCounter.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class StudentCounter{
-    public static void main(String... args){
+    public static void main(String... args) {
         ArrayList<Map<String, Object>> map = new ArrayList<Map<String, Object>>();
 
         // Given this list of hashmaps...
@@ -10,43 +10,68 @@ public class StudentCounter{
         row0.put("name", "Rezso");
         row0.put("age", 9.5);
         row0.put("candies", 2);
-        map.add( row0 );
+        map.add(row0);
 
         Map<String, Object> row1 = new HashMap<String, Object>();
         row1.put("name", "Gerzson");
         row1.put("age", 10);
         row1.put("candies", 1);
-        map.add( row1 );
+        map.add(row1);
 
         Map<String, Object> row2 = new HashMap<String, Object>();
         row2.put("name", "Aurel");
         row2.put("age", 7);
         row2.put("candies", 3);
-        map.add( row2 );
+        map.add(row2);
 
         Map<String, Object> row3 = new HashMap<String, Object>();
         row3.put("name", "Zsombor");
         row3.put("age", 12);
         row3.put("candies", 5);
-        map.add( row3 );
+        map.add(row3);
 
         Map<String, Object> row4 = new HashMap<String, Object>();
         row4.put("name", "Olaf");
         row4.put("age", 12);
         row4.put("candies", 7);
-        map.add( row4 );
+        map.add(row4);
 
         Map<String, Object> row5 = new HashMap<String, Object>();
         row5.put("name", "Teodor");
         row5.put("age", 3);
         row5.put("candies", 2);
-        map.add( row5 );
+        map.add(row5);
 
         // Display the following things:
         //  - Who has got more candies than 4 candies
         //  - Sum the age of people who have lass than 5 candies
 
+        int[] numberCandies = new int[6];
 
-        System.out.println(map.get(0).get("candies"));
+        for (int i = 0; i <= 5; i++) {
+            numberCandies[i] = (Integer) (map.get(i).get("candies"));
+        }
+
+        for (int i = 0; i <= 5; i++) {
+            if (numberCandies[i] > 4) {
+                System.out.println(map.get(i).get("name"));
+            }
+        }
+
+        double[] numberAges = new double[6];
+        double sumAges = 0;
+
+        for (int i = 0; i <= 5; i++) {
+            String convert = map.get(i).get("age").toString();
+            numberAges[i] = Double.parseDouble(convert);
+        }
+
+        for (int i = 0; i <= 5; i++) {
+            if (numberCandies[i] < 5) {
+                sumAges = sumAges + numberAges[i];
+            }
+        }
+
+        System.out.println(sumAges);
     }
 }
