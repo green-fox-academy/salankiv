a509c3326be00525143a715da109d8b89ee1fd77
Viktor Salanki <salankiv5@gmail.com>
2017-09-19 11:54:51 +0200

    CountLines task solved

--- week-03/day-2/src/CountLines.java
+++ week-03/day-2/src/CountLines.java
@@ -1,4 +1,28 @@
-// Write a function that takes a filename as string,
-// then returns the number of lines the file contains.
-// It should return zero if it can't open the file, and
-// should not raise any error.
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLines {
+	public static void main(String[] args) {
+		// Write a function that takes a filename as string,
+		// then returns the number of lines the file contains.
+		// It should return zero if it can't open the file, and
+		// should not raise any error.
+
+		System.out.println(numberOfLines("encoded-lines.txt"));
+
+	}
+
+	private static int numberOfLines(String fileName) {
+		try {
+			Path myPath = Paths.get(fileName);
+			List<String> lines = Files.readAllLines(myPath);
+			int linesNumber = lines.size();
+			return linesNumber;
+		} catch (IOException e) {
+			return 0;
+		}
+	}
+}
