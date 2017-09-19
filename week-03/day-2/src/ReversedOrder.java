336e254681c235fde1d822f486d7570547717d6a
Viktor Salanki <salankiv5@gmail.com>
2017-09-19 16:05:50 +0200

    ReversedOrder task solved

--- week-03/day-2/src/ReversedOrder.java
+++ week-03/day-2/src/ReversedOrder.java
@@ -1,8 +1,43 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReversedOrder {
+  public static void main(String[] args) {
+    // Create a method that decrypts reversed-order.txt
 
-    public static void main(String[] args) {
-        // Create a method that decrypts reversed-order.txt
-
-        
+    for (String lines : reverser("src/reversed-order.txt")) {
+      System.out.println(lines);
     }
+
+  }
+
+  public static List<String> reverser(String fileName) {
+    Path filePath = Paths.get(fileName);
+    String singledLine = "";
+    List<String> singledText = new ArrayList<>();
+    try {
+      List<String> content = Files.readAllLines(filePath);
+      for (int i = content.size() - 1; i >= 0; i--) {
+        String line = content.get(i);
+        List<Character> characters = new ArrayList<>();
+        for (int j = 0; j < line.length(); j++) {
+          char c = (char) line.charAt(j);
+          characters.add(c);
+        }
+        for (int k = 0; k < characters.size(); k++) {
+          singledLine = singledLine + characters.get(k);
+        }
+        singledText.add(singledLine);
+        singledLine = "";
+      }
+      return singledText;
+    } catch (IOException e) {
+      System.out.println("Could not find file");
+    }
+    return null;
+  }
 }
