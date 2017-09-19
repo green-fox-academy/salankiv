db1e0d0c4efb21fb72b5cf95874f3cde571cc396
Viktor Salanki <salankiv5@gmail.com>
2017-09-19 12:46:27 +0200

    CopyFile task solved

--- week-03/day-2/src/CopyFile.java
+++ week-03/day-2/src/CopyFile.java
@@ -1,3 +1,29 @@
-// Write a function that copies a file to an other
-// It should take the filenames as parameters
-// It should return a boolean that shows if the copy was successful
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CopyFile {
+	public static void main(String[] args) {
+		// Write a function that copies a file to an other
+		// It should take the filenames as parameters
+		// It should return a boolean that shows if the copy was successful
+
+
+		System.out.println(copyFile("my-file.txt", "myNewFile.txt"));
+
+	}
+
+	private static boolean copyFile(String file1, String file2) {
+		Path firstFile = Paths.get(file1);
+		Path secondFile = Paths.get(file2);
+		try {
+			List<String> content = Files.readAllLines(firstFile);
+			Files.write(secondFile, content);
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+}
