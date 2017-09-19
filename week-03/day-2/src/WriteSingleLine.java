b7b0b7fe9e0a2bcc54b3328f6fb284065302e79b
Viktor Salanki <salankiv5@gmail.com>
2017-09-19 12:13:47 +0200

    WriteSingleLine task solved

--- week-03/day-2/src/WriteSingleLine.java
+++ week-03/day-2/src/WriteSingleLine.java
@@ -1,6 +1,9 @@
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteSingleLine {
   public static void main(String[] args) {
@@ -9,6 +12,16 @@ public class WriteSingleLine {
     // If the program is unable to write the file,
     // then it should print an error message like: "Unable to write file: my-file.txt"
 
+		List<String> myName = new ArrayList<>();
+		myName.add("Viktor Salanki");
+		String fileName = "my-file.txt";
+		try {
+			Path myPath = Paths.get(fileName);
+			Files.write(myPath, myName);
+			System.out.println(Files.readAllLines(myPath));
+		} catch (IOException e) {
+			System.out.println("Unable to write file: " + fileName);
+		}
 
   }
 }
