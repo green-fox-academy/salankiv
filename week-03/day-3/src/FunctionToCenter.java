a5cd65d26c2ccecfcf54a4cc218c15b545bafa40
Viktor Salanki <salankiv5@gmail.com>
2017-09-20 16:24:24 +0200

    finished FunctionToCenter

--- week-03/day-3/src/FunctionToCenter.java
+++ week-03/day-3/src/FunctionToCenter.java
@@ -1,22 +1,36 @@
 import javax.swing.*;
-
 import java.awt.*;
-
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class FunctionToCenter {
+	public static void mainDraw(Graphics graphics){
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a line from that point to the center of the canvas.
+    // fill the canvas with lines from the edges, every 20 px, to the center.
 
-    public static void mainDraw(Graphics graphics){
-      // create a line drawing function that takes 2 parameters:
-      // the x and y coordinates of the line's starting point
-      // and draws a line from that point to the center of the canvas.
-      // fill the canvas with lines from the edges, every 20 px, to the center.
+		for (int i = 0; i < WIDTH; i += 20) {
+			lineDrawing(i, 0, graphics);
+		}
 
+		for (int i = 0; i < WIDTH; i += 20) {
+			lineDrawing(i, HEIGHT, graphics);
+		}
 
+		for (int i = 0; i < HEIGHT; i += 20) {
+			lineDrawing(0, i, graphics);
+		}
 
-    }
+		for (int i = 0; i < HEIGHT; i += 20) {
+			lineDrawing(WIDTH, i, graphics);
+		}
+  }
 
-//    Don't touch the code below
+	private static void lineDrawing(int x, int y, Graphics graphics) {
+		graphics.drawLine(x, y, WIDTH / 2, HEIGHT / 2);
+	}
+
+	//    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
     public static void main(String[] args) {
