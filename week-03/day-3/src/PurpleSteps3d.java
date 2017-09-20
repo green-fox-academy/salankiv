ee1d93950859058fc11df87b305a3c6b6f7dfe47
Viktor Salanki <salankiv5@gmail.com>
2017-09-20 15:50:01 +0200

    finished PurpleSteps3d

--- week-03/day-3/src/PurpleSteps3d.java
+++ week-03/day-3/src/PurpleSteps3d.java
@@ -1,18 +1,24 @@
 import javax.swing.*;
-
 import java.awt.*;
-
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class PurpleSteps3d {
+	public static void mainDraw(Graphics graphics){
+    // reproduce this:
+    // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
 
-    public static void mainDraw(Graphics graphics){
-      // reproduce this:
-      // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
+		int size = 10;
+		int x = 10;
+		int y = 10;
+		graphics.setColor(new Color(198,0,237));
+		for (int i = 0; i < 6; i++) {
+			x = x + i * 10;
+			y = y + i * 10;
+			graphics.fillRect(x, y, size, size);
+			size += 10;
+		}
+	}
 
-
-
-    }
 
 //    Don't touch the code below
     static int WIDTH = 320;
