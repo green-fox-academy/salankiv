f070d3deb0a50ac947bc34abaa0b66c2ce657a90
Viktor Salanki <salankiv5@gmail.com>
2017-09-20 12:34:24 +0200

    finished CenterBoxFunction

--- week-03/day-3/src/CenterBoxFunction.java
+++ week-03/day-3/src/CenterBoxFunction.java
@@ -1,20 +1,26 @@
 import javax.swing.*;
-
 import java.awt.*;
-
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class CenterBoxFunction {
+	public static void mainDraw(Graphics graphics){
+    // create a square drawing function that takes 1 parameter:
+    // the square size
+    // and draws a square of that size to the center of the canvas.
+    // draw 3 squares with that function.
 
-    public static void mainDraw(Graphics graphics){
-      // create a square drawing function that takes 1 parameter:
-      // the square size
-      // and draws a square of that size to the center of the canvas.
-      // draw 3 squares with that function.
+		squareDrawing(50, graphics);
+		squareDrawing(100, graphics);
+		squareDrawing(10, graphics);
+	}
 
+	public static void squareDrawing(int size, Graphics graphics) {
+		graphics.drawLine((WIDTH / 2) - size / 2,(HEIGHT / 2) - size / 2, (WIDTH / 2) + size / 2, (HEIGHT / 2) - size / 2);
+		graphics.drawLine((WIDTH / 2) + size / 2,(HEIGHT / 2) - size / 2, (WIDTH / 2) + size / 2, (HEIGHT / 2) + size / 2);
+		graphics.drawLine((WIDTH / 2) - size / 2,(HEIGHT / 2) + size / 2, (WIDTH / 2) + size / 2, (HEIGHT / 2) + size / 2);
+		graphics.drawLine((WIDTH / 2) - size / 2,(HEIGHT / 2) - size / 2, (WIDTH / 2) - size / 2, (HEIGHT / 2) + size / 2);
 
-
-    }
+   }
 
 //    Don't touch the code below
     static int WIDTH = 320;
