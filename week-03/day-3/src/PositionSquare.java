2f886aaf35480c37f885fe7fecc099b6ac1342ed
Viktor Salanki <salankiv5@gmail.com>
2017-09-20 12:27:13 +0200

    finished PositionSquare

--- week-03/day-3/src/PositionSquare.java
+++ week-03/day-3/src/PositionSquare.java
@@ -1,22 +1,27 @@
 import javax.swing.*;
-
 import java.awt.*;
-
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class PositionSquare {
+	public static void mainDraw(Graphics graphics){
+    // create a square drawing function that takes 2 parameters:
+    // the x and y coordinates of the square's top left corner
+    // and draws a 50x50 square from that point.
+    // draw 3 squares with that function.
 
-    public static void mainDraw(Graphics graphics){
-      // create a square drawing function that takes 2 parameters:
-      // the x and y coordinates of the square's top left corner
-      // and draws a 50x50 square from that point.
-      // draw 3 squares with that function.
-
-
-
+		squareDrawing(10, 10, graphics);
+		squareDrawing(100, 10, graphics);
+		squareDrawing(10, 100, graphics);
     }
 
-//    Don't touch the code below
+	public static void squareDrawing(int x, int y, Graphics graphics) {
+		graphics.drawLine(x, y, x + 50, y);
+		graphics.drawLine(x + 50, y, x + 50, y + 50);
+		graphics.drawLine(x, y + 50, x + 50, y + 50);
+		graphics.drawLine(x, y, x, y + 50);
+	}
+
+	//    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
 
