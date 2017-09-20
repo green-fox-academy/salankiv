f67d9c317398792d56efcb8257e036e92eddb745
Viktor Salanki <salankiv5@gmail.com>
2017-09-20 10:41:51 +0200

    finished ColoredBox

--- week-03/day-3/src/ColoredBox.java
+++ week-03/day-3/src/ColoredBox.java
@@ -1,17 +1,23 @@
 import javax.swing.*;
-
 import java.awt.*;
-
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class ColoredBox {
+  public static void mainDraw(Graphics graphics){
+    // draw a box that has different colored lines on each edge.
 
-    public static void mainDraw(Graphics graphics){
-      // draw a box that has different colored lines on each edge.
+		graphics.setColor(Color.GREEN);
+		graphics.drawLine(10, 10, 110, 10);
 
+		graphics.setColor(Color.RED);
+		graphics.drawLine(110, 10, 110, 110);
 
+		graphics.setColor(Color.BLACK);
+		graphics.drawLine(10, 110, 110, 110);
 
-    }
+		graphics.setColor(Color.BLUE);
+		graphics.drawLine(10, 10, 10, 110);
+  }
 
 //    Don't touch the code below
     static int WIDTH = 320;
