2e0e83454e55068068bca8dabbe5d72666b39af6
Viktor Salanki <salankiv5@gmail.com>
2017-09-20 18:06:08 +0200

    finished LinePlayQuarters

--- week-03/day-3/src/LinePlayQuarters.java
+++ week-03/day-3/src/LinePlayQuarters.java
@@ -1,19 +1,53 @@
 import javax.swing.*;
-
 import java.awt.*;
-
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class LinePlayQuarters {
+	public static void mainDraw(Graphics graphics){
+		// divide the canvas into 4 parts
+    // and repeat this pattern in each quarter:
+    // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 
-    public static void mainDraw(Graphics graphics){
-      // divide the canvas into 4 parts
-      // and repeat this pattern in each quarter:
-      // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
+		graphics.setColor(Color.DARK_GRAY);
+		for (int i = 1; i < 16 ; i++) {
+			graphics.drawLine(i * 10, 5, 155, i * 10);
+		}
 
+		graphics.setColor(Color.GREEN);
+		for (int i = 1; i < 16 ; i++) {
+			graphics.drawLine(5, i * 10, i * 10, 168);
+		}
 
+		graphics.setColor(Color.DARK_GRAY);
+		for (int i = 1; i < 16 ; i++) {
+			graphics.drawLine(i * 10, 173, 155, 173 + i * 10);
+		}
 
-    }
+		graphics.setColor(Color.GREEN);
+		for (int i = 1; i < 16 ; i++) {
+			graphics.drawLine(5, 173 + i * 10, i * 10, 338);
+		}
+
+		graphics.setColor(Color.DARK_GRAY);
+		for (int i = 1; i < 16 ; i++) {
+			graphics.drawLine(155 + i * 10, 173, 315, 173 + i * 10);
+		}
+
+		graphics.setColor(Color.GREEN);
+		for (int i = 1; i < 16 ; i++) {
+			graphics.drawLine(155, 173 + i * 10, 155 + i * 10, 338);
+		}
+
+		graphics.setColor(Color.DARK_GRAY);
+		for (int i = 1; i < 16 ; i++) {
+			graphics.drawLine(155 + i * 10, 5, 315, i * 10);
+		}
+
+		graphics.setColor(Color.GREEN);
+		for (int i = 1; i < 16 ; i++) {
+			graphics.drawLine(155, i * 10, 155 + i * 10, 168);
+		}
+}
 
 //    Don't touch the code below
     static int WIDTH = 320;
