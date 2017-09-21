7dc932ca8988fb6b3d76deb4a3758a35175853da
Viktor Salanki <salankiv5@gmail.com>
2017-09-21 13:27:59 +0200

    finished String3

--- week-03/day-4/src/String3.java
+++ week-03/day-4/src/String3.java
@@ -1,2 +1,16 @@
+public class String3 {
+	public static void main(String[] args) {
+		System.out.println(changeXChars ("abcdxxxefghxxx"));
+	}
+
+	private static String changeXChars(String string) {
+		if (string.length() == 0) {
+			return "";
+		} else {
+			return changeXChars(string.substring(0, string.length() - 1)) + string.substring(string.length() - 1, string.length()) + "*";
+		}
+	}
+}
+
 // Given a string, compute recursively a new string where all the
 // adjacent chars are now separated by a "*".
