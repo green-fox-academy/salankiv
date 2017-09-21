eda242854a7419a0e68ca5947445802691e202bc
Viktor Salanki <salankiv5@gmail.com>
2017-09-21 12:16:52 +0200

    finished String1

--- week-03/day-4/src/String1.java
+++ week-03/day-4/src/String1.java
@@ -1,2 +1,18 @@
+public class String1 {
+	public static void main(String[] args) {
+		System.out.println(changeXChars ("abcdxxxefghxxx"));
+	}
+
+	private static String changeXChars(String string) {
+		if (string.length() == 0) {
+			return "";
+		} else if (string.charAt(string.length() - 1) == 'x') {
+				return changeXChars(string.substring(0, string.length() - 1)) + "y";
+		} else {
+				return changeXChars(string.substring(0, string.length() - 1)) + string.substring(string.length() - 1, string.length());
+		}
+	}
+}
+
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
