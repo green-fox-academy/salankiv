d77445a10cdcc0c3f7fff2e996d67c401590c87a
Viktor Salanki <salankiv5@gmail.com>
2017-09-21 10:38:16 +0200

    finished NumberAdder

--- week-03/day-4/src/NumberAdder.java
+++ week-03/day-4/src/NumberAdder.java
@@ -1,1 +1,14 @@
+public class NumberAdder {
+	public static void main(String[] args) {
+		System.out.println(numberAdder(100));
+	}
+
+	private static int numberAdder(int number) {
+		if (number == 0) {
+			return 0;
+		}
+		System.out.println(number);
+		return numberAdder(number - 1) + number;
+	}
+}
 // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
