7803be6e174cf84ecde668c11ad6f73d1833b3f7
Viktor Salanki <salankiv5@gmail.com>
2017-09-21 15:11:54 +0200

    finished Fibonacci

--- week-03/day-4/src/Fibonacci.java
+++ week-03/day-4/src/Fibonacci.java
@@ -1,3 +1,21 @@
+public class Fibonacci {
+	public static void main(String[] args) {
+
+		System.out.println(fibonacci(7));
+	}
+
+	private static int fibonacci(int number) {
+		if (number == 0) {
+			return 0;
+		} else if (number == 1) {
+			return 1;
+		}	else {
+				int prev = fibonacci(number -2);
+				return fibonacci(number - 1) + prev;
+		}
+	}
+}
+
 // The fibonacci sequence is a famous bit of mathematics, and it happens to
 // have a recursive definition. The first two values in the sequence are
 // 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the
