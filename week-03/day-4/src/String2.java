192356e1b26631254e02191cc10568ef1c3b9533
Viktor Salanki <salankiv5@gmail.com>
2017-09-21 12:40:29 +0200

    finished String2

--- week-03/day-4/src/String2.java
+++ week-03/day-4/src/String2.java
@@ -1,1 +1,17 @@
+public class String2 {
+	public static void main(String[] args) {
+		System.out.println(changeXChars ("abcdxxxefghxxx"));
+	}
+
+	private static String changeXChars(String string) {
+		if (string.length() == 0) {
+			return "";
+		} else if (string.charAt(string.length() - 1) == 'x') {
+			return changeXChars(string.substring(0, string.length() - 1)) + "";
+		} else {
+			return changeXChars(string.substring(0, string.length() - 1)) + string.substring(string.length() - 1, string.length());
+		}
+	}
+}
+
 // Given a string, compute recursively a new string where all the 'x' chars have been removed.
