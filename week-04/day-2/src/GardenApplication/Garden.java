ce08e6fe0f3bc065833b6b41ad6fc7fffc9de505
Viktor Salanki <salankiv5@gmail.com>
2017-09-26 16:13:00 +0200

    creating Garden sceleton

---
+++ week-04/day-2/src/GardenApplication/Garden.java
@@ -0,0 +1,20 @@
+package GardenApplication;
+
+import java.util.List;
+
+public class Garden {
+	List<Tree> trees;
+	List<Flower> flowers;
+
+	public Garden(){
+	}
+
+	public void watering() {
+	//	when watering it should only water those what needs water with equally divided amount amongst them
+	//	eg. watering with 40 and 4 of them need water then each gets watered with 10
+	}
+
+	public void status() {
+	// sout
+	}
+}
