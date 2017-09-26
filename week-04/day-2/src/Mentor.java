97aa11eb91dcb4f9a7b1a969d80c799039b82d5f
Viktor Salanki <salankiv5@gmail.com>
2017-09-26 14:41:16 +0200

    creating Mentor class

---
+++ week-04/day-2/src/Mentor.java
@@ -0,0 +1,25 @@
+public class Mentor extends Person {
+	String level;
+
+	@Override
+	public void getGoal() {
+		System.out.println("Educate brilliant junior software developers.");
+	}
+
+	@Override
+	public void introduce() {
+		System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " " + level + " mentor.");
+	}
+
+	public Mentor(String name, int age, String gender, String level) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.level = level;
+	}
+
+	public Mentor() {
+		this.level = "intermediate";
+	}
+	
+}
