24ebc704bf631c50d51b09a995432ca514d9f986
Viktor Salanki <salankiv5@gmail.com>
2017-09-26 14:11:06 +0200

    creating Student class

---
+++ week-04/day-2/src/Student.java
@@ -0,0 +1,27 @@
+public class Student extends Person {
+	String previousOrganization;
+	int skippedDays;
+
+	@Override
+	public void getGoal() {
+		System.out.println("Be a junior software developer.");
+	}
+
+	@Override
+	public void introduce() {
+		System.out.println("Hi, I'm " + name + ", a " + age +" year old " + gender + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+	}
+
+	public void skipDays(int numberOfDays) {
+		this.skippedDays += numberOfDays;
+	}
+
+	public Student(String name, int age, String gender, String previousOrganization) {
+		this.skippedDays = 0;
+	}
+
+	private Student() {
+		this.previousOrganization = "The School of Life";
+		this.skippedDays = 0;
+	}
+}
