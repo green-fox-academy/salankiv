ad34d45900a5ac881a7de209f502d11b1fe15c14
Viktor Salanki <salankiv5@gmail.com>
2017-09-26 19:28:18 +0200

    creating status feedback

--- week-04/day-2/src/GardenApplication/Garden.java
+++ week-04/day-2/src/GardenApplication/Garden.java
@@ -5,17 +5,23 @@ import java.util.List;
 
 public class Garden {
 	List<Plant> plants;
+	int counterFlowers;
+	int counterTrees;
 
 	public Garden() {
 		plants = new ArrayList<>();
+		counterFlowers = 0;
+		counterTrees = 0;
 	}
 
 	public void addTree(Tree newTree) {
 		plants.add(newTree);
+		counterTrees += 1;
 	}
 
 	public void addFlower(Flower newFlower) {
 		plants.add(newFlower);
+		counterFlowers += 1;
 	}
 
 	public void watering() {
@@ -24,20 +30,26 @@ public class Garden {
 	}
 
 	public void status() {
+		System.out.println("In the garden there are " + counterFlowers + " flowers and " + counterTrees + " trees.");
 		for (int i = 0; i < plants.size(); i++) {
-			System.out.println(plants.get(i).name);
+			if (plants.get(i).thirsty) {
+				System.out.println("The " + plants.get(i).color + " " + plants.get(i).name + " needs water.");
+			} else {
+				System.out.println("The " + plants.get(i).color + " " + plants.get(i).name + " doesn't need water.");
+			}
 		}
-
 	}
 
 	public static void main(String[] args) {
 
 		Garden exampleGarden = new Garden();
-		Flower flower1 = new Flower("orange");
-		Tree tree1 = new Tree("black");
-		Tree tree2 = new Tree("white");
+		Flower flower1 = new Flower("yellow");
+		Flower flower2 = new Flower("blue");
+		Tree tree1 = new Tree("purple");
+		Tree tree2 = new Tree("orange");
 
 		exampleGarden.addFlower(flower1);
+		exampleGarden.addFlower(flower2);
 		exampleGarden.addTree(tree1);
 		exampleGarden.addTree(tree2);
 
