8af7280d8ae8f679baa3b27e516dc6730251cbc1
Viktor Salanki <salankiv5@gmail.com>
2017-09-30 15:15:44 +0200

    Anagram with working for loop

--- week-04/day-3/src/Anagram.java
+++ week-04/day-3/src/Anagram.java
@@ -1,36 +1,28 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Anagram {
 	String string1;
 	String string2;
-	ArrayList<Character> charList = new ArrayList<>();
-	String orderdString1;
-	String orderedString2;
 
 	public boolean checkAnagram(String string1, String string2) {
-		charList.clear();
-		System.out.println(splitToChar(string1));
-		orderdString1 = splitToChar(string1);
-		charList.clear();
-		orderedString2 = splitToChar(string2);
-		return orderdString1 == orderedString2;
+		return sort(string1).equals(sort(string2));
 	}
 
-	public String splitToChar(String string) {
-		if (string.length() == 1) {
-			return string.substring(0,1);
-		} else {
-				charList.add(string.charAt(string.length()-1));
-				Collections.sort(charList);
-				return splitToChar(string.substring(0, string.length()-1)).toString();
+	private String sort(String string) {
+		StringBuilder sb = new StringBuilder(string);
+		for (int i = 0; i < sb.length(); i++) {
+			for (int j = 0; j < sb.length(); j++) {
+				if (sb.charAt(j) < sb.charAt(i)) {
+					sb.insert(i, sb.charAt(j));
+					sb.deleteCharAt(j + 1);
+				}
+			}
 		}
+		return sb.toString();
 	}
+
 
 	public static void main(String[] args) {
 		Anagram myAnagram = new Anagram();
-		System.out.println(myAnagram.checkAnagram("dog", "god"));
+		System.out.println(myAnagram.checkAnagram("alma", "mlaa"));
 	}
 
 }
