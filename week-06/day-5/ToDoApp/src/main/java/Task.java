1e96a3ac633e85cd59c61d4e6dbca09939bb7009
Viktor Salanki <salankiv5@gmail.com>
2017-10-20 13:40:08 +0200

    Added time feat to Task class

--- week-06/day-5/ToDoApp/src/main/java/Task.java
+++ week-06/day-5/ToDoApp/src/main/java/Task.java
@@ -1,26 +1,24 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Task {
 	private static int taskCounter;
 
 	public String name;
-	public boolean completed;
 	public int id;
-	public LocalDate createdDate;
-	public LocalDate completedDate;
+	public LocalDateTime createdDate;
+	public LocalDateTime completedDate;
 
 	public Task(String name) {
 		this.name = name;
-		this.completed = false;
 		taskCounter++;
 		this.id = taskCounter;
-		this.createdDate = LocalDate.now();
+		this.createdDate = LocalDateTime.now();
 		this.completedDate = null;
 	}
 
 	public void setCompleted() {
-		this.completed = true;
-		this.completedDate = LocalDate.now();
+		this.completedDate = LocalDateTime.now();
 	}
 
 	public int completionTime() {
