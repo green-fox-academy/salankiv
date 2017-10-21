e239a451d8375a2ce65fe17b2f2fcaad6d3b48bb
Viktor Salanki <salankiv5@gmail.com>
2017-10-21 16:43:46 +0200

    Adding and greeting functions ready

--- week-06/day-5/ToDoApp/src/main/java/ArgumentHandler.java
+++ week-06/day-5/ToDoApp/src/main/java/ArgumentHandler.java
@@ -1,34 +1,49 @@
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
+import java.util.List;
+
 public class ArgumentHandler {
 
-	public static void checkArguments(String[] args) {
+	public static void checkArguments(String[] args, List<Task> taskList) {
 
-		OptionParser parser = new OptionParser();
-		parser.accepts("l");
-		parser.accepts("a").withRequiredArg();
-		parser.accepts("r").withRequiredArg();
-		parser.accepts("c");
-		parser.accepts("u");
-		OptionSet options = parser.parse(args);
+			OptionParser parser = new OptionParser();
+			parser.accepts("l");
+			parser.accepts("a").withRequiredArg();
+			parser.accepts("r").withRequiredArg();
+			parser.accepts("c");
+			parser.accepts("u");
+			OptionSet options = parser.parse(args);
 
-		if (options.has("l")) {
+			if (!options.hasOptions()) {
+				System.out.println
+						("\n" +
+						"Command Line Todo application\n" +
+						"=============================\n" +
+						"\n" +
+						"Command line arguments:\n" +
+						"-l   Lists all the tasks\n" +
+						"-a   Adds a new task\n" +
+						"-r   Removes a task\n" +
+						"-c   Completes a task\n" +
+						"-u   Updates a task\n");
+			}
+			if (options.has("l")) {
+				System.out.println("list");
+			}
+			if (options.has("a")) {
+				addTask((String) options.valueOf("a"), taskList);
+			}
+			if (options.has("r")) {
+				removeTask((String) options.valueOf("r"));
+			}
+			if (options.has("c")) {
 
-		}
-		if (options.has("a")) {
-			addTask((String) options.valueOf("a"));
-		}
-		if (options.has("r")) {
-			removeTask((String) options.valueOf("r"));
-		}
-		if (options.has("c")) {
+			}
+			if (options.has("u")) {
 
+			}
 		}
-		if (options.has("u")) {
-
-		}
-	}
 
 	public void greeting() {
 
@@ -38,8 +53,9 @@ public class ArgumentHandler {
 
 	}
 
-	public static void addTask(String taskName) {
+	public static void addTask(String taskName, List<Task> taskList) {
 		Task newTask = new Task(taskName);
+		taskList.add(newTask);
 	}
 
 	public static void removeTask(String taskName) {
--- week-06/day-5/ToDoApp/src/main/java/Main.java
+++ week-06/day-5/ToDoApp/src/main/java/Main.java
@@ -1,17 +1,16 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		List<Task> taskList = new ArrayList<>();
+		List<Task> taskList;
 		taskList = FileManupilation.loadAll();
-		ArgumentHandler.checkArguments(args);
+		ArgumentHandler.checkArguments(args, taskList);
 
-		for (Task t : taskList) {
-			System.out.println(t.name);
-		}
+//		for (Task t : taskList) {
+//			System.out.println(t.name);
+//		}
 
 	}
 }
