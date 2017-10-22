9f9dc605d5cc82169d586b573ef319f0c378b811
Viktor Salanki <salankiv5@gmail.com>
2017-10-22 13:19:55 +0200

    Update task function ready

--- week-06/day-5/ToDoApp/src/main/java/ArgumentHandler.java
+++ week-06/day-5/ToDoApp/src/main/java/ArgumentHandler.java
@@ -8,33 +8,33 @@ public class ArgumentHandler {
 
 	public static void checkArguments(String[] args, List<Task> taskList) {
 
-			OptionParser parser = new OptionParser();
-			parser.accepts("l");
-			parser.accepts("a").withRequiredArg();
-			parser.accepts("r").withRequiredArg();
-			parser.accepts("c").withRequiredArg();
-			parser.accepts("u");
-			OptionSet options = parser.parse(args);
+		OptionParser parser = new OptionParser();
+		parser.accepts("l");
+		parser.accepts("a").withRequiredArg();
+		parser.accepts("r").withRequiredArg();
+		parser.accepts("c").withRequiredArg();
+		parser.accepts("u").withRequiredArg().withValuesSeparatedBy(',');
+		OptionSet options = parser.parse(args);
 
-			if (!options.hasOptions()) {
-				greeting();
-			}
-			if (options.has("l")) {
-				listTasks();
-			}
-			if (options.has("a")) {
-				addTask((String) options.valueOf("a"), taskList);
-			}
-			if (options.has("r")) {
-				removeTask((String) options.valueOf("r"));
-			}
-			if (options.has("c")) {
-				completeTask((String) options.valueOf("c"));
-			}
-			if (options.has("u")) {
-
-			}
+		if (!options.hasOptions()) {
+			greeting();
 		}
+		if (options.has("l")) {
+			listTasks();
+		}
+		if (options.has("a")) {
+			addTask((String) options.valueOf("a"), taskList);
+		}
+		if (options.has("r")) {
+			removeTask((String) options.valueOf("r"));
+		}
+		if (options.has("c")) {
+			completeTask((String) options.valueOf("c"));
+		}
+		if (options.has("u")) {
+			updateTask((List<?>) options.valuesOf("u"));
+		}
+	}
 
 	public static void greeting() {
 		System.out.println
@@ -69,12 +69,26 @@ public class ArgumentHandler {
 		for (String[] l : lines) {
 			if (!l[0].equals(taskName)) {
 				newLines.add(l);
-				}
 			}
+		}
 		FileManupilation.saveAll(newLines);
 	}
 
 	public static void completeTask(String id) {
 		FileManupilation.load(id);
+	}
+
+	public static void updateTask(List<?> args) {
+		List<String[]> lines = FileManupilation.readFile();
+		List<String[]> newLines = new ArrayList<>();
+		for (int i = 0; i < lines.size(); i++) {
+			if (!lines.get(i)[1].equals(args.get(0))) {
+				newLines.add(lines.get(i));
+			} else {
+				lines.get(i)[0] = args.get(1).toString();
+				newLines.add(lines.get(i));
+			}
+		}
+		FileManupilation.saveAll(newLines);
 	}
 }
