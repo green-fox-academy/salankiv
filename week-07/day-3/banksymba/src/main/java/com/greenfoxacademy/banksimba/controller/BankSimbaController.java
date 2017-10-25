85da90da110dc0bab3e1ad2eaf9ee2aa44ce28f6
Viktor Salanki <salankiv5@gmail.com>
2017-10-25 14:12:18 +0200

    Multiple accounts ready

--- week-07/day-3/banksymba/src/main/java/com/greenfoxacademy/banksimba/controller/BankSimbaController.java
+++ week-07/day-3/banksymba/src/main/java/com/greenfoxacademy/banksimba/controller/BankSimbaController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 public class BankSimbaController {
@@ -21,5 +24,17 @@ public class BankSimbaController {
 		String stringToCheck = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
 		model.addAttribute("string", stringToCheck);
 		return "htmlception";
+	}
+
+	@RequestMapping(value = "/bank")
+	public String bank(Model model) {
+		List<BankAccount> bank = new ArrayList<>();
+		bank.add(new BankAccount("Simba", 2000, "lion"));
+		bank.add(new BankAccount("Zordon", 3000, "lion"));
+		bank.add(new BankAccount("Rafiki", 5000, "monkey"));
+		bank.add(new BankAccount("Zazu", 1000, "bird"));
+		bank.add(new BankAccount("Timon", 3500, "other"));
+		model.addAttribute("list", bank);
+		return "bank";
 	}
 }
