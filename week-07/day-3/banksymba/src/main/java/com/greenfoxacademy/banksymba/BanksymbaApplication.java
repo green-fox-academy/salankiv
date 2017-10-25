8ffdcb11107d5276e6f273690de02798984e4f24
Viktor Salanki <salankiv5@gmail.com>
2017-10-25 11:05:00 +0200

    Creating project structure

---
+++ week-07/day-3/banksymba/src/main/java/com/greenfoxacademy/banksymba/BanksymbaApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.banksymba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksymbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanksymbaApplication.class, args);
+	}
+}
