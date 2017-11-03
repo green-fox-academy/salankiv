dffae21ad168714d8f6864b832153e7979a485d2
Viktor Salanki <salankiv5@gmail.com>
2017-11-03 10:26:07 +0100

    Adding post feature ready

--- week-08/day-5/redditproject/src/main/java/com/greenfox/salankiv/redditproject/controller/PostController.java
+++ week-08/day-5/redditproject/src/main/java/com/greenfox/salankiv/redditproject/controller/PostController.java
@@ -1,10 +1,12 @@
 package com.greenfox.salankiv.redditproject.controller;
 
+import com.greenfox.salankiv.redditproject.model.Post;
 import com.greenfox.salankiv.redditproject.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/posts")
@@ -20,6 +22,12 @@ public class PostController {
 
 	@GetMapping(value = {"/add"})
 	public String addPost() {
+		return "addpost";
+	}
+
+	@GetMapping(value = {"/add/save"})
+	public String savePost(@RequestParam String content) {
+		postRepository.save(new Post(content));
 		return "addpost";
 	}
 
