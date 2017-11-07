6568885358df8fb8be9c232b280172399a2cf397
Viktor Salanki <salankiv5@gmail.com>
2017-11-07 10:50:30 +0100

    Restcontroller project structure and frontend ready

---
+++ week-09/day-2/restcontroller/src/main/java/com/greenfox/salankiv/restcontroller/RestcontrollerApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.salankiv.restcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcontrollerApplication.class, args);
+	}
+}
